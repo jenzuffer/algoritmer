fa1e14673b2dd79d269038155223fa6da0fcb6e8
Christian Moustesgård <cph-cm267@cphbusiness.dk>
2021-05-10 11:44:07 +0200

    bfs implementation

--- src/main/java/Algoritmer/BFS.java
+++ src/main/java/Algoritmer/BFS.java
@@ -1,6 +1,7 @@
 package Algoritmer;
 
 import Implementation.queue.Queue;
+import Interfaces.Edge;
 import Interfaces.Graph;
 
 public class BFS {
@@ -8,15 +9,25 @@ public class BFS {
     private boolean[] marked;
     private Queue pq;
 
+    //Time Complexity: O(V+E) where V is number of vertices in the graph and E is number of edges
     public BFS(Graph graph, int startNode){
-
+        this.marked = new boolean[graph.getVertiesCount()];
         marked[startNode] = true;
         pq = new Queue();
         pq.enqueue(startNode);
 
         while (!pq.isEmpty()){
-
+            int dequeue = (int) pq.dequeue();
+            System.out.println("Dequeue: " + dequeue);
+            for (Edge edge : graph.adj(dequeue)) {
+                float weight = edge.getWeight();
+                int to = edge.to();
+                if (!marked[to]){
+                    marked[to] = true;
+                    pq.enqueue(to);
+                    System.out.println("weight: " + weight + " to: " + to);
+                }
+            }
         }
-
     }
 }
